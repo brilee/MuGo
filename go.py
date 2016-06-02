00d14d1a92f8577c3bbe72b419d79e6f90b1c767
Brian Lee <brian.kihoon.lee@gmail.com>
2016-06-02 11:44:43 -0400

    Add Group definition and group update routines

--- go.py
+++ go.py
@@ -1,12 +1,13 @@
 import re
+from collections import namedtuple
 
 N = 9
 W = N + 1
 # Represent a board as a string, with '.' empty, 'X' to play, 'x' other player.
 # Whitespace is used as a border (to avoid IndexError when computing neighbors)
 
-# A Coordinate `c` is (Integer index | None) an index into the board.
-# None can indicate lack of a ko restriction or a pass move.
+# A Coordinate `c` is an int: an index into the board.
+# A Move is a (Coordinate c | None).
 
 # Human-readable move notation: columns go from A to T left to right
 # rows go from 1 to 19 from bottom to top.
@@ -42,4 +43,44 @@ def neighbors(c):
 
 def place_stone(board, move, c):
     return board[:c] + move + board[c+1:]
+
+class Group(namedtuple('Group', 'stones liberties')):
+    '''
+    stones: a set of Coordinates belonging to this group
+    liberties: a set of Coordinates that are empty and adjacent to this group.
+    '''
+    pass
+
+def update_groups(board, existing_groups, c):
+    '''
+    When a move is played, update the list of groups and their liberties.
+    This means possibly appending the new move to a group, creating a new 1-stone group, or merging existing groups.
+    The existing groups and the new move are assumed to be of the same color.
+    The board should represent the state after the move has been played at `c`.
+    '''
+    move_color = board[c]
+    assert move_color in 'xX'
+    move_neighbors = neighbors(c)
+    adjacent_stones = [n for n in move_neighbors if board[n] == move_color]
+    new_liberties = set(n for n in move_neighbors if board[n] == '.')
+
+    updated_groups = []
+    groups_to_merge = []
+    for g in existing_groups:
+        if any(s in g.stones for s in adjacent_stones):
+            groups_to_merge.append(g)
+        else:
+            updated_groups.append(g)
+
+    if groups_to_merge:
+        new_stones = set([c])
+        new_liberties = new_liberties
+        for g in groups_to_merge:
+            new_stones = new_stones | g.stones
+            new_liberties = new_liberties | g.liberties
+        new_liberties.remove(c)
+        updated_groups.append(Group(stones=new_stones, liberties=new_liberties))
+    else:
+        updated_groups.append(Group(stones=set([c]), liberties=new_liberties))
+    return updated_groups
 
--- tests/test_go.py
+++ tests/test_go.py
@@ -18,7 +18,7 @@ class TestGoBoard(unittest.TestCase):
     def test_load_board(self):
         self.assertEqual(len(go.EMPTY_BOARD), (go.W * (go.W + 1)))
         self.assertEqual(go.EMPTY_BOARD, MANUAL_EMPTY_BOARD)
-        self.assertEqual(go.EMPTY_BOARD, go.load_board('.' * go.N ** 2))
+        self.assertEqual(go.EMPTY_BOARD, go.load_board('. \n' * go.N ** 2))
 
     def test_parsing(self):
         self.assertEqual(go.parse_coords('A' + str(go.N)), go.W)
@@ -31,3 +31,34 @@ class TestGoBoard(unittest.TestCase):
         side = go.parse_coords('A2')
         side_neighbors = [go.EMPTY_BOARD[c] for c in go.neighbors(side)]
         self.assertEqual(sum(1 for n in side_neighbors if n.isspace()), 1)
+
+class TestGroupHandling(unittest.TestCase):
+    def test_update_groups(self):
+        board = go.load_board('''
+            .X.......
+            X........
+            .........
+            .........
+            .........
+            .........
+            .........
+            .........
+            .........
+        ''')
+        pc = go.parse_coords
+        existing_groups = [
+            go.Group(
+                stones=set([pc('B9')]),
+                liberties=set([pc('A9'), pc('C9'), pc('B8')])
+            ),
+            go.Group(
+                stones=set([pc('A8')]),
+                liberties=set([pc('A9'), pc('A7'), pc('B8')])
+            )
+        ]
+        updated_board = go.place_stone(board, 'X', go.parse_coords('A9'))
+        updated_groups = go.update_groups(updated_board, existing_groups, go.parse_coords('A9'))
+        self.assertEqual(updated_groups, [go.Group(
+            stones=set([pc('A8'), pc('A9'), pc('B9')]),
+            liberties=set([pc('A7'), pc('B8'), pc('C9')])
+        )])
