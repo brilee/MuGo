1bd0c84b6924e17ab539a2901447f1313e52e8a3
Brian Lee <brian.kihoon.lee@gmail.com>
2016-06-09 12:03:02 -0400

    fix bug in initial_state

--- go.py
+++ go.py
@@ -195,7 +195,7 @@ class Position(namedtuple('Position', 'board n komi caps groups ko last last2 pl
     '''
     @staticmethod
     def initial_state():
-        return Position(EMPTY_BOARD, n=0, komi=7.5, caps=(0, 0), groups=(set(), set()), ko=None, last=None, last2=None)
+        return Position(EMPTY_BOARD, n=0, komi=7.5, caps=(0, 0), groups=(set(), set()), ko=None, last=None, last2=None, player1turn=True)
 
     def possible_moves(self):
         return [c for c in ALL_COORDS if self.board[c] == '.' and not is_likely_eye(self.board, c)]
