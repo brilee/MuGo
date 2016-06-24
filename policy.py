cd2998ab6f2139e4ffb003ec214901984e8f2836
Brian Lee <brian.kihoon.lee@gmail.com>
2016-06-24 12:57:51 -0400

    reformat policy network code

--- policy.py
+++ policy.py
@@ -28,55 +28,75 @@ from load_data_sets import load_data_sets
 
 kgs = load_data_sets([features.StoneColorFeature, features.LibertyFeature], "kgs-micro")
 
-k = 64
-num_intermediate_conv_layers = 3
+class PolicyNetwork(object):
+    def __init__(self, num_input_planes, k=64, num_int_conv_layers=3):
+        self.num_input_planes = num_input_planes
+        self.k = k
+        self.num_int_conv_layers = num_int_conv_layers
+        self.set_up_network()
 
-x = tf.placeholder(tf.float32, [None, go.N, go.N, kgs.input_planes])
-y = tf.placeholder(tf.float32, shape=[None, go.N ** 2])
+    def set_up_network(self):
+        x = tf.placeholder(tf.float32, [None, go.N, go.N, self.num_input_planes])
+        y = tf.placeholder(tf.float32, shape=[None, go.N ** 2])
 
-#convenience functions for initializing weights and biases
-def weight_variable(shape):
-    return tf.Variable(tf.truncated_normal(shape, stddev=0.1))
+        #convenience functions for initializing weights and biases
+        def weight_variable(shape, name):
+            return tf.Variable(tf.truncated_normal(shape, stddev=0.1), name=name)
 
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding="SAME")
+        def conv2d(x, W):
+            return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding="SAME")
 
-# initial conv layer is 5x5
-W_conv_init = weight_variable([5, 5, kgs.input_planes, k])
-h_conv_init = tf.nn.relu(conv2d(x, W_conv_init))
+        # initial conv layer is 5x5
+        W_conv_init = weight_variable([5, 5, self.num_input_planes, self.k], name="W_conv_init")
+        h_conv_init = tf.nn.relu(conv2d(x, W_conv_init))
 
-# followed by a series of 3x3 conv layers
-W_conv_intermediate = []
-h_conv_intermediate = []
-current_h_conv = h_conv_init
-for i in range(num_intermediate_conv_layers):
-    W_conv_intermediate.append(weight_variable([3, 3, k, k]))
-    h_conv_intermediate.append(tf.nn.relu(conv2d(current_h_conv, W_conv_intermediate[-1])))
-    current_h_conv = h_conv_intermediate[-1]
+        # followed by a series of 3x3 conv layers
+        W_conv_intermediate = []
+        h_conv_intermediate = []
+        _current_h_conv = h_conv_init
+        for i in range(self.num_int_conv_layers):
+            W_conv_intermediate.append(weight_variable([3, 3, self.k, self.k], name="W_conv_inter" + str(i)))
+            h_conv_intermediate.append(tf.nn.relu(conv2d(_current_h_conv, W_conv_intermediate[-1])))
+            _current_h_conv = h_conv_intermediate[-1]
 
-W_conv_final = weight_variable([1, 1, k, 1])
-b_conv_final = tf.Variable(tf.constant(0, shape=[go.N ** 2], dtype=tf.float32))
-h_conv_final = conv2d(h_conv_intermediate[-1], W_conv_final)
-output = tf.nn.softmax(tf.reshape(h_conv_final, [-1, go.N ** 2]) + b_conv_final)
+        W_conv_final = weight_variable([1, 1, self.k, 1], name="W_conv_final")
+        b_conv_final = tf.Variable(tf.constant(0, shape=[go.N ** 2], dtype=tf.float32), name="b_conv_final")
+        h_conv_final = conv2d(h_conv_intermediate[-1], W_conv_final)
+        output = tf.nn.softmax(tf.reshape(h_conv_final, [-1, go.N ** 2]) + b_conv_final)
 
-log_likelihood_cost = -tf.reduce_mean(tf.reduce_sum(tf.mul(tf.log(output), y), reduction_indices=[1]))
+        log_likelihood_cost = -tf.reduce_mean(tf.reduce_sum(tf.mul(tf.log(output), y), reduction_indices=[1]))
 
-train_step = tf.train.AdamOptimizer(1e-4).minimize(log_likelihood_cost)
-was_correct = tf.equal(tf.argmax(output, 1), tf.argmax(y, 1))
-accuracy = tf.reduce_mean(tf.cast(was_correct, tf.float32))
+        train_step = tf.train.AdamOptimizer(1e-4).minimize(log_likelihood_cost)
+        was_correct = tf.equal(tf.argmax(output, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(was_correct, tf.float32))
+        # save everything to self.
+        for name, thing in locals().items():
+            if not name.startswith('_'):
+                setattr(self, name, thing)
 
-# Train!
-sess = tf.Session()
-sess.run(tf.initialize_all_variables())
-for i in range(10000):
-    batch_x, batch_y = kgs.training.get_batch(16)
-    if i % 10 == 0:
-        train_accuracy = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y})
-        print("Step %d, training data accuracy: %g" % (i, train_accuracy))
-    if i % 1000 == 0:
-        test_accuracy = sess.run(accuracy, feed_dict={x: kgs.test.input, y: kgs.test.labels})
-        print("Step %d, test data accuracy: %g" % (i, test_accuracy))
-    sess.run(train_step, feed_dict={x: batch_x, y: batch_y})
+    def initialize_variables(self):
+        self.session = tf.Session()
+        # put loading functionality here
+        self.session.run(tf.initialize_all_variables())
 
-test_accuracy = sess.run(accuracy, feed_dict={x: kgs.test.input, y: kgs.test.labels})
-print("Final test data accuracy: %g" % test_accuracy)
+    def train(self, training_data, batch_size=16):
+        num_minibatches = training_data.data_size // batch_size
+        for i in range(num_minibatches):
+            batch_x, batch_y = training_data.get_batch(batch_size)
+            if i % 10 == 0:
+                train_accuracy = self.session.run(self.accuracy, feed_dict={self.x: batch_x, self.y: batch_y})
+                print("Step %d, training data accuracy: %g" % (i, train_accuracy))
+            self.session.run(self.train_step, feed_dict={self.x: batch_x, self.y: batch_y})
+
+    def execute(self, input):
+        self.session.run(self.output, feed_dict={self.x: input})
+
+    def check_accuracy(self, test_data):
+        test_accuracy = self.session.run(self.accuracy, feed_dict={self.x: test_data.input, self.y: test_data.labels})
+        print("Test data accuracy: %g" % test_accuracy)
+
+n = PolicyNetwork(kgs.input_planes)
+n.initialize_variables()
+for i in range(10):
+    n.train(kgs.training)
+    n.check_accuracy(kgs.test)
