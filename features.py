021e064de1180c7c972bc8bda35bbb0619e941d8
Brian Lee <brian.kihoon.lee@gmail.com>
2016-08-30 11:32:47 -0400

    Emphasize importance of ones feature

--- features.py
+++ features.py
@@ -2,6 +2,8 @@
 Features used by AlphaGo, in approximate order of importance.
 Feature                 # Notes
 Stone colour            3 Player stones; oppo. stones; empty  
+Ones                    1 Constant plane of 1s 
+    (Because of convolution w/ zero-padding, this is the only way the NN can know where the edge of the board is!!!)
 Turns since last move   8 How many turns since a move played
 Liberties               8 Number of liberties
 Capture size            8 How many opponent stones would be captured
@@ -11,7 +13,6 @@ ladder capture          1 Whether a move is a successful ladder cap
 Ladder escape           1 Whether a move is a successful ladder escape
 Sensibleness            1 Whether a move is legal + doesn't fill own eye
 Zeros                   1 Constant plane of 0s
-Ones                    1 Constant plane of 1s
 
 All features with 8 planes are 1-hot encoded, with plane i marked with 1 
 only if the feature was equal to i. Any features >= 8 would be marked as 8.
@@ -45,6 +46,10 @@ def stone_color_feature(position):
     features[board == go.WHITE, 1] = 1
     features[board == go.EMPTY, 2] = 1
     return features
+
+@planes(1)
+def ones_feature(position):
+    return np.ones([go.N, go.N, 1], dtype=np.float32)
 
 @planes(P)
 def recent_move_feature(position):
@@ -83,6 +88,7 @@ class FeatureExtractor(object):
 
 DEFAULT_FEATURES = FeatureExtractor([
     stone_color_feature,
+    ones_feature,
     liberty_feature,
     recent_move_feature,
     would_capture_feature,
