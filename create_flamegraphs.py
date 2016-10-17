e798c59e9e3a3b8abbfc49511d0ff65ef93ecc0a
Brian Lee <brian.kihoon.lee@gmail.com>
2016-10-17 12:54:18 -0400

    Add code to log processing time

--- create_flamegraphs.py
+++ create_flamegraphs.py
@@ -1,6 +1,6 @@
 import subprocess, os, time
 
-opts = [{'compression': c, 'packing': p} for c in ['snappy'] for p in ['none', 'half', 'full']]
+opts = [{'compression': c, 'packing': p} for p in ['none', 'half', 'full'] for c in ['none', 'gzip9', 'gzip6', 'snappy']]
 
 def extract_write_pack_percents(prof):
     with open(prof) as f:
@@ -8,8 +8,10 @@ def extract_write_pack_percents(prof):
     total_traces = sum(int(l[1]) for l in lines)
     write_traces = [int(l[1]) for l in lines if l[0].endswith('write') or l[0].endswith('compress')]
     pack_traces = [int(l[1]) for l in lines if l[0].endswith('pack')]
-    return sum(write_traces) / total_traces, sum(pack_traces) / total_traces
-
+    pack = sum(pack_traces)
+    write = sum(write_traces)
+    remainder = total_traces - pack - write
+    return remainder / total_traces, pack / total_traces, write / total_traces
 
 
 def run_options(compression, packing):
@@ -20,9 +22,10 @@ def run_options(compression, packing):
     subprocess.call("../Flamegraph/flamegraph.pl {}.prof > {}.svg".format(filename, filename), shell=True)
     compressed_filesize = os.path.getsize('processed_data/test.chunk.gz') + os.path.getsize('processed_data/train0.chunk.gz')
     total_time = tock - tick
-    write_frac, pack_frac = extract_write_pack_percents("{}.prof".format(filename))
-    print("%s\t%s\t%.2f\t%.2f\t%d" % (compression, packing, write_frac * total_time, pack_frac * total_time, compressed_filesize))
+    remainder_frac, write_frac, pack_frac = extract_write_pack_percents("{}.prof".format(filename))
+    print("%s\t%s\t%.2f\t%.2f\t%.2f\t%d" % (packing, compression, remainder_frac * total_time, write_frac * total_time, pack_frac * total_time, compressed_filesize))
 
 for opt_set in opts:
+    print("packing | compression | processing | bitpack | compress+write | output size")
     run_options(**opt_set)
 
