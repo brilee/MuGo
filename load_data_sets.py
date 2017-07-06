6d63747dba4f4e01b46bc1e1ccfef45c596fd74f
Brian Lee <brian.kihoon.lee@gmail.com>
2017-07-05 22:46:37 -0400

    Fix float in shuffle kwarg

--- load_data_sets.py
+++ load_data_sets.py
@@ -28,7 +28,7 @@ def iter_chunks(chunk_size, iterator):
         else:
             break
 
-def shuffler(iterator, pool_size=1e5, refill_threshold=0.9):
+def shuffler(iterator, pool_size=10**5, refill_threshold=0.9):
     yields_between_refills = round(pool_size * (1 - refill_threshold))
     # initialize pool; this step may or may not exhaust the iterator.
     pool = take_n(pool_size, iterator)
@@ -72,7 +72,7 @@ def split_test_training(positions_w_context, est_num_positions):
         test_size = len(positions_w_context) // 3
         return positions_w_context[:test_size], [positions_w_context[test_size:]]
     else:
-        shuffled_positions = shuffler(shuffled_positions)
+        shuffled_positions = shuffler(positions_w_context)
         test_chunk = take_n(desired_test_size, shuffled_positions)
         training_chunks = iter_chunks(CHUNK_SIZE, shuffled_positions)
         return test_chunk, training_chunks
